f1a24f9853c8ec55431c6bd960c263d689c2fe98
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-06-10 23:42:21 +0530

    feat: file structuring implemented

---
+++ CityOnboarding/Core/HomeScreen/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  CityOnboarding
+//
+//  Created by Prashanna Rajbhandari on 10/06/2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ CityOnboarding/Core/OnBoarding/View/OnBoarding.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoarding.swift
+//  CityOnboarding
+//
+//  Created by Prashanna Rajbhandari on 10/06/2023.
+//
+
+import SwiftUI
+
+struct OnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoarding()
+}
