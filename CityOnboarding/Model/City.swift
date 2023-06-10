6769ddb421c3944fb5efb3b749d8ca56d887aaf0
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-06-11 03:22:35 +0530

    feat: data model added and updated assets

---
+++ CityOnboarding/Model/City.swift
@@ -0,0 +1,19 @@
+//
+//  City.swift
+//  CityOnboarding
+//
+//  Created by Prashanna Rajbhandari on 11/06/2023.
+//
+
+import Foundation
+
+
+struct City : Codable, Identifiable{
+    let id : String
+    let imageName : String?
+    let title : String?
+    let desc : String?
+}
+
+
+
